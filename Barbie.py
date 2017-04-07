17086077b6cc2caf34bc4dfd7e46720d6c09db5d
julioalvesMS <julioalvesMS@users.noreply.github.com>
2017-04-07 11:07:14 -0300

    Several bug fixes

--- Barbie.py
+++ Barbie.py
@@ -23,7 +23,6 @@ class SusyHTMLParser(HTMLParser):
 				print("File OUT:", attrs[0][1])
 				self.res_files.append(attrs[0][1])
 			else:
-				print()
 				print("File IN: ", attrs[0][1])
 				self.in_files.append(attrs[0][1])
 			self.out = not self.out
@@ -47,16 +46,15 @@ def get_susy_files(susy_link):
 
 	return parser.get_in_files(), parser.get_res_files()
 
+
 def download_file(url, file_name):
 	# Download the file from `url` and save it locally under `file_name`:
 	with open(file_name, 'w') as out_file:
 		data = requests.get(url, verify=False).text # a `bytes` object
 		out_file.write(data)
 
-
 def download_tests(susy_tests_url, in_files, res_files, dir_name):
 	susy_path = susy_tests_url.split('/')[:-1]
-	#local_path = susy_path[3:]
 
 	try:
 		shutil.rmtree(dir_name)
@@ -69,19 +67,27 @@ def download_tests(susy_tests_url, in_files, res_files, dir_name):
 	print()
 	try:
 		for i in range(len(in_files)):
+
+			os.mkdir(os.path.join(dir_name, str(i+1)))
+			
+			# download test input
 			print('Downloading file %d of %d' %(i*2+1, len(in_files)*2), end='\r')
-			download_file(data_url + in_files[i], dir_name + in_files[i])
+			download_file(data_url + in_files[i], os.path.join(dir_name, str(i+1), in_files[i]))
+			
+			# Download test output
 			print('Downloading file %d of %d' %(i*2+2, len(in_files)*2), end='\r')
-			download_file(data_url + res_files[i], dir_name + res_files[i])
+			download_file(data_url + res_files[i], os.path.join(dir_name, str(i+1), res_files[i]))
+	
 	except Exception as e:
 		print('Connection Failed!')
 		print('Error:', e)
 		print()
 		sys.exit('Exiting program')
 
-def exe_code(exec_file, in_file, out_dir):
-	out = open(out_dir + in_file.split('.')[0] + '.out', 'w')
-	ins = open(out_dir + in_file, 'r')
+
+def exe_code(exec_file, in_file, out_dir, index):
+	out = open(os.path.join(out_dir, str(index), in_file.split('.')[0]) + '.out', 'w')
+	ins = open(os.path.join(out_dir, str(index), in_file), 'r')
 	process = subprocess.Popen(exec_file, stdin=ins, stdout=out, stderr=subprocess.STDOUT, universal_newlines=True)
 	out.close()
 	ins.close()
@@ -91,7 +97,7 @@ def run_tests(exec_file, in_files, out_dir):
 	print()
 	for i in range(size):
 		print('Executing test %d of %d' %(i+1, size), end='\r')
-		exe_code(exec_file, in_files[i], out_dir)
+		exe_code(exec_file, in_files[i], out_dir, i+1)
 	print()
 
 
@@ -106,8 +112,8 @@ def compare_susy(susy_file, out_file, index):
 
 		diference_count = 0
 
-		msg_1 = "Susy file: %s with %d lines" % (susy_file, len(s_lines))
-		msg_2 = "Test file: %s with %d lines" % (out_file, len(t_lines))
+		msg_1 = "Susy file: %s with %d lines\n" % (susy_file, len(s_lines))
+		msg_2 = "Test file: %s with %d lines\n" % (out_file, len(t_lines))
 
 		barbie_out.write(msg_1)
 		barbie_out.write(msg_2)
@@ -123,13 +129,13 @@ def compare_susy(susy_file, out_file, index):
 
 				diference_count += 1
 
-		msg_f =  "Diference count: %d" % diference_count
+		msg_f =  "\nDiference count: %d" % diference_count
 		barbie_out.write(msg_f)
 
 		if not diference_count:
 			msg_t = "Teste %d: Resultado correto!" % (index)
 		else:
-			msg_t = "Teste %d: Resultado incorreto. Linhas divergentes: %d" % (index, 1 + diference_count + abs(len(s_lines), len(t_lines)))
+			msg_t = "Teste %d: Resultado incorreto. Linhas divergentes: %d" % (index, diference_count + abs(len(s_lines) - len(t_lines)))
 
 		print(msg_t,)
 
@@ -137,23 +143,27 @@ def compare_susy(susy_file, out_file, index):
 	susy.close()
 	test.close()
 
+
 def main():
 
 	exec_file = sys.argv[1]
 	url = sys.argv[2]
 
-	tests_dir_name = "dados/"
+	tests_dir_name = "testes/"
 
 	in_files, res_files = get_susy_files(url)
 	download_tests(url, in_files, res_files, tests_dir_name)
 	run_tests(exec_file, in_files, tests_dir_name)
 	for arq in res_files:
-		print()
 		test = arq.split('.')[0]
-		compare_susy(os.path.join(tests_dir_name, arq), os.path.join(tests_dir_name, test) + '.out', res_files.index(arq))
+		index  = res_files.index(arq)+1
+		compare_susy(os.path.join(tests_dir_name, str(index), arq), os.path.join(tests_dir_name, str(index), test) + '.out', index)
 
 
 if __name__=="__main__":
+
+	# Ignore Susy Certificate
 	from requests.packages.urllib3.exceptions import InsecureRequestWarning
 	requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+	
 	main()
