4bebf2616f34d20a9d6582202f23ac309f9b62e7
Julio <julioalves.mesquita@gmail.com>
2018-07-07 16:34:21 -0300

    Facilitada a chamada pelo barbie web

--- barbie.py
+++ barbie.py
@@ -8,6 +8,7 @@ import susy_interface as susy
 from barbie_test import BarbieTest
 from barbie_exceptions import UnknownLanguageException
 from codes import PythonCode, CCode, CodeLanguage
+from barbie_call import BarbieCall
 import log
 
 def get_local_tests(tests_folder):
@@ -61,80 +62,12 @@ def get_language_and_compile(files):
 		raise UnknownLanguageException
 	return user_code
 
-def usage():
-	options = []
-	log.uprint("""\
-Barbie!
-The Susy Simulator
-
-usage: python3 barbie.py [-h] [-e] [-u] [-l] [-t] [Arquivos de código]
-
-Opções:
-			Mostra as opções de linha de comando
--e,	--executable	Executa a barbie utilizando um arquivo
-						executavel, de um código já compilado
--u, --url			Fornecer diretamente o link da página
-						com os arquivos de teste do susy
--l, --local			Não acessa o Susy e faz os testes com os
-						arquivos já baixados anteriormente
--t, --timeout		Define o timeout de execução de cada teste
-						o valor deve ser dado em segundos
-by: Mosquito""")
-
-
-def main():
-
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "he:u:lt:", ["help", "executable=","url=", "local", "timeout"])
-	except getopt.GetoptError as err:
-		# print help information and exit:
-		eprint(err)  # will print something like "option -a not recognized"
-		usage()
-		exit(2)
-
-	exec_file = None
-	url = None
-	source_code = None
-	local = False
-	timeout = 2
-
-	for o, a in opts:
-		if o in ("-h", "--help"):
-			usage()
-			exit()
-		elif o in ("-e", "--executable"):
-			exec_file = os.path.realpath(a)
-		elif o in ("-u", "--url"):
-			url = a
-		elif o in ("-l", "--local"):
-			local = True
-		elif o in ("-t", "--timeout"):
-			timeout = int(a)
-		else:
-			assert False, "unhandled option"
-
-	_files = dict()
-	for path in args:
-		# Get extension
-		ext = os.path.splitext(path)[1][1:].strip()
-		if ext not in _files:
-			_files[ext] = list()
-		# Save in the dict the path to this file
-		_files[ext].append(os.path.realpath(path))
-
-
-	# We need to receive at least a
-	if (not exec_file and not _files):
-		usage()
-		exit(2)
-
-	user_code = None
-
+def prepare_code(files, executable_file=None):
 	# If the user got us one, compile the source code
-	if _files:
+	if files:
 		# Try to compile the source code
 		try:
-			user_code = get_language_and_compile(_files)
+			user_code = get_language_and_compile(files)
 			if type(user_code) is CCode:
 				assert user_code.compilation_sucess, "Falha na compilação"
 				log.uprint('Código compilado com sucesso!')
@@ -151,9 +84,32 @@ def main():
 			with open(user_code.gcc_file, 'r') as gcc:
 				log.eprint(gcc.read())
 			exit(1)
-	elif exec_file:
+	elif executable_file:
 		user_code = CodeLanguage();
-		user_code.exec_file = exec_file
+		user_code.exec_file = executable_file
+	return user_code
+
+def usage():
+	log.uprint("""\
+Barbie:
+O seu Susy local
+
+usage: python3 barbie.py [-h] [-e] [-u] [-l] [-t] [Arquivos de código]
+
+Opções:
+-e, --executable    Executa a barbie utilizando um arquivo
+                    executavel, de um código já compilado.
+-u, --url           Fornecer diretamente o link da página
+                    com os arquivos de teste do susy.
+-l, --local         Não acessa o Susy e faz os testes com os
+                    arquivos já baixados anteriormente.
+-t, --timeout       Define o timeout de execução de cada teste
+                    o valor deve ser dado em segundos.
+""")
+
+
+def execute(barbie_call):
+	user_code = prepare_code(barbie_call.code_files, barbie_call.executable_file)
 
 	tests_dir_name = os.path.realpath("testes/")
 	in_files = None
@@ -161,33 +117,77 @@ def main():
 
 	# If the program is not set to run locally and no url was given,
 	# prompt user for the class info and discover the url
-	if not url and not local:
-
-		disc  =	input("Disciplina: 	")
-		turma = input("Turma: 		")
-		lab   = input("Lab: 		")
-		url = susy.discover_susy_url(disc, turma, lab)
+	if not barbie_call.test_files_url and not barbie_call.local:
+		barbie_call.test_files_url = susy.discover_susy_url(barbie_call.disciplina, barbie_call.turma, barbie_call.codigo_lab)
 
 	# The user may input the url from the desired tests page
-	if url:
+	if barbie_call.test_files_url:
 		# List all susy files of open tests
-		in_files, res_files = susy.get_susy_files(url)
+		in_files, res_files = susy.get_susy_files(barbie_call.test_files_url)
 		# Download all the open tests
-		susy.download_tests(url, in_files, res_files, tests_dir_name)
+		susy.download_tests(barbie_call.test_files_url, in_files, res_files, tests_dir_name)
 
 	# Option to run the program locally, and don't acess Susy
-	if local:
+	if barbie_call.local:
 		in_files, res_files = get_local_tests(tests_dir_name)
-
 
 	# If we sucessufuly got all needed files,
 	# we may run all tests and compare our output with the expected
 	if in_files and res_files:
-		run_and_compare(user_code, in_files, res_files, tests_dir_name, timeout)
+		run_and_compare(user_code, in_files, res_files, tests_dir_name, barbie_call.timeout)
 	exit()
+
+def main():
+
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "he:u:lt:", ["help", "executable=","url=", "local", "timeout"])
+	except getopt.GetoptError as err:
+		# print help information and exit:
+		eprint(err)  # will print something like "option -a not recognized"
+		usage()
+		exit(2)
+
+	barbie_call = BarbieCall()
+
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			usage()
+			exit()
+		elif o in ("-e", "--executable"):
+			barbie_call.executable_file = os.path.realpath(a)
+		elif o in ("-u", "--url"):
+			barbie_call.test_files_url = a
+		elif o in ("-l", "--local"):
+			barbie_call.local = True
+		elif o in ("-t", "--timeout"):
+			barbie_call.timeout = double(a)
+		else:
+			assert False, "unhandled option"
+
+	barbie_call.code_files = dict()
+	for path in args:
+		# Get extension
+		ext = os.path.splitext(path)[1][1:].strip()
+		if ext not in barbie_call.code_files:
+			barbie_call.code_files[ext] = list()
+		# Save in the dict the path to this file
+		barbie_call.code_files[ext].append(os.path.realpath(path))
+
+	# We need to receive at least a
+	if (not barbie_call.executable_file and not barbie_call.code_files):
+		usage()
+		exit(2)
+
+	if not barbie_call.test_files_url and not barbie_call.local:
+		barbie_call.disciplina   = input("Disciplina: ")
+		barbie_call.turma        = input("Turma:      ")
+		barbie_call.codigo_lab   = input("Lab:        ")
+
+	execute(barbie_call)
+
 
 if __name__=="__main__":
 	log.supress_output = False
 	main()
 else:
-	log.supress_output = True
+	log.supress_output = False
---
+++ barbie_call.py
@@ -0,0 +1,12 @@
+
+class BarbieCall:
+    def __init__(self):
+        self.code_files = None
+        self.executable_file = None
+        self.timeout = 2
+        self.local = False
+        self.test_files_url = None
+
+        self.disciplina = None
+        self.turma = None
+        self.codigo_lab = None
